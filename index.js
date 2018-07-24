d3f3860f8db3a2ec0e527282957b074650b9d734
Matheus Valenza <matheusvvalenza@gmail.com>
2018-07-23 21:48:37 -0300

    first question prototype

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-$("#get-btn").click(function(){
+$("#get-all").click(function() {
     $.getJSON("https://swapi.co/api/people", function(data, status){
-        console.log("Data: " + data + "\nStatus: " + status);
+        // console.log("Data: " + data + "\nStatus: " + status);
         var items = [];
-        $.each( data, function( key, val ) {
+        console.log(data);
+        $.each( data["results"], function( key, val ) {
 			console.log(key, val);
-			items.push( "<li id='" + key + "'>" + val + "</li>" );
+			items.push( "<li id='" + key + "'>" + "(" + val["url"] + ")" + val["name"] + "</li>" );
 		});
 
 		$( "<ul/>", {
@@ -12,4 +13,66 @@ $("#get-btn").click(function(){
 			html: items.join( "" )
 		}).appendTo( "body" );
     });
+});
+
+$("#get-by-id").click(function() {
+	var randomId = Math.floor((Math.random() * 87) + 1);
+
+    $.getJSON(`https://swapi.co/api/people/${randomId}`, function(data, status){
+        // console.log("Data: " + data + "\nStatus: " + status);
+        var items = [];
+        console.log(data);
+        $.each( data, function( key, val ) {
+			console.log(key, val);
+			items.push( "<li id='" + key + "'>" + key + ": " + val + "</li>" );
+		});
+
+		$( "<ul/>", {
+			"class": "section",
+			html: items.join( "" )
+		}).appendTo( "body" );
+    });
+});
+
+$("#question-1").click(function() {
+	var randomId = Math.floor((Math.random() * 87) + 1);
+
+	console.log(randomId);
+	console.log("generate-question");
+
+    $.getJSON(`https://swapi.co/api/people/${randomId}`, function(data, status){
+
+        var items = [];
+        console.log(data);
+        $.each( data, function( key, val ) {
+			console.log(key, val);
+			items.push( "<li id='" + key + "'>" + key + ": " + val + "</li>" );
+
+			if (key === "name") {
+				$("#question").append(`Qual a cor dos olhos de ${val} ?`);
+			}
+
+			if (key === "eye_color") {
+				localStorage.setItem("correctAnswer", val);
+			}
+		});
+
+		$( "<ul/>", {
+			"class": "section",
+			html: items.join( "" )
+		}).appendTo( "body" );
+    }); 
+
+    $("#form").append('<input class="input is-2" id="answer" type="text" placeholder="Text input">');
+});
+
+$("#submit").click(function(event)	 {
+	console.log("submit: ", $( "#answer" ));
+	if ( $( "#answer" ).val() === localStorage.getItem("correctAnswer") ) {
+	    console.log("brown");
+	    return;
+  	}
+ 
+	event.preventDefault();
+	console.log("not brown");
 });
