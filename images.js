35325160ed3caa31f0a62690992bfe7822738eaf
Aron Richter <aronrichter@gmail.com>
2018-07-24 21:00:57 -0300

    busca de imagens bing

---
+++ images.js
@@ -0,0 +1,20 @@
+$("#request-image").click(function() {
+    let request = new XMLHttpRequest();
+    let url = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=darth";
+    let key = "fe69342813854c00b235c9ac7f12cefb";
+
+    request.open("GET", url);
+    request.setRequestHeader("Ocp-Apim-Subscription-Key", key);
+    request.setRequestHeader("Accept", "application/json");
+
+    request.addEventListener("load", function(){
+        if (this.status === 200) {
+            console.log("returnGet");
+        }
+    });
+    
+    request.send();
+
+    return console.log("endFunction");
+});
+
